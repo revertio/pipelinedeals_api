648d52c060a3d923791274e079d9327a7d5f5faa
Rich Chetwynd <richardchetwynd@gmail.com>
2014-07-22 16:46:05 +1200

    removed comments. they're already there.

--- lib/pipeline_deals/resources/note.rb
+++ lib/pipeline_deals/resources/note.rb
@@ -5,6 +5,5 @@ module PipelineDeals
     belongs_to :deal, class_name: PipelineDeals::Deal
     belongs_to :person, class_name: PipelineDeals::Person
     belongs_to :created_by_user, class_name: PipelineDeals::User, foreign_key: :created_by_user_id
-    has_many :comments, class_name: PipelineDeals::Comment
   end
 end
